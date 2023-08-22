c42ad4ae02d35d584da8bb321ee51c67012025ea
shivnath chavan <61450813+shivnathchavan@users.noreply.github.com>
2023-08-22 16:43:41 +0530

    Delete Readme.js

--- Readme.js
+++
@@ -1,9 +0,0 @@
-// This is a simple example of a readme.js file
-
-// Function to be executed when the page loads
-function onPageLoad() {
-  alert("Welcome to my profile!");
-}
-
-// Attach the function to the "load" event of the window
-window.addEventListener("load", onPageLoad);
