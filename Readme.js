19f5456e765fff9e422f18bcd228a53951c77615
shivnath chavan <61450813+shivnathchavan@users.noreply.github.com>
2023-08-22 12:51:13 +0530

    Create Readme.js

---
+++ Readme.js
@@ -0,0 +1,24 @@
+<html>
+<head>
+<style>
+  body {
+    background-color: transparent;
+  }
+  .flex-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .left-container {
+    flex: 1;
+  }
+  .right-container {
+    flex: 1;
+    text-align: right;
+  }
+  .center-container {
+    text-align: center;
+  }
+</style>
+</head>
+<body>
